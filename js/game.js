c1b467596aede8c59a94fc6bc9b139ae7a31f3da
Melanie Hahn <melaniesarah@gmail.com>
2016-08-08 15:17:16 -0700

    Add slide part of move function'

--- js/game.js
+++ js/game.js
@@ -47,19 +47,50 @@ Game.prototype.generateBoard = function() {
 }
 
 Game.prototype.move = function(direction){
-
+  // var storageArray = this.nestedArray();
+  var tempArray = this.nestedArray();
+  if (direction === 'right') {
+    // start with the first row
+    console.log(this.nestedArray());
+    for (i=0; i<=3; i++) {
+      var count = 0;
+      while (tempArray[i][3] === 0 && count < 4) {
+        tempArray[i][3] = tempArray[i][2];
+        tempArray[i][2] = tempArray[i][1];
+        tempArray[i][1] = tempArray[i][0];
+        tempArray[i][0] = 0;
+        count += 1;
+      }
+      count = 0;
+      while (tempArray[i][2] === 0 && count < 3) {
+        tempArray[i][2] = tempArray[i][1];
+        tempArray[i][1] = tempArray[i][0];
+        tempArray[i][0] = 0;
+        count += 1;
+      }
+      if (tempArray[i][1] === 0) {
+        tempArray[i][1] = tempArray[i][0];
+        tempArray[i][0] = 0;
+      }
+    console.log(tempArray);
+    }
+  }
 }
 
 function nextChar(c) {
     return String.fromCharCode(c.charCodeAt(0) + 1);
 }
 
+// game = new Game();
+// console.log(game.nestedArray());
+// console.log(game.toHash());
+
+// move logic - start with swipe right
+// start with the first row
+//  if number on far right side is non-zero, leave in place
+// else shift all numbers over one place, put a zero at the far left side and move on to 2nd position
+
+
 game = new Game();
-console.log(game.nestedArray());
-console.log(game.toHash());
-
-
-
-
-
+game.move('right');
 
