c2fd1c23eac83f2e5a2976fa62e841b8ff920337
Melanie Hahn <melaniesarah@gmail.com>
2016-08-08 12:34:25 -0700

    Change variable naming convention

--- js/game.js
+++ js/game.js
@@ -5,6 +5,7 @@ var Game = function(boardString) {
   } else {
     boardArray = this.generateBoard();
   }
+  this.boardArray = boardArray;
   this.boardHash = this.toHash(boardArray);
 }
 
@@ -12,27 +13,27 @@ Game.prototype.generateBoard = function() {
   var myArray = [2,4];
   var number;
   var index;
-  var BoardArray = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+  var boardArray = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
 
   for (i=1; i<=2; i++) {
     // sample from myArray (can only populate the board with a 2 or 4)
     number = myArray[Math.floor(Math.random() * 2)];
     // sample from board array to put location of number
     index = Math.floor(Math.random() * 16);
-    BoardArray[index] = number;
+    boardArray[index] = number;
    }
-   return BoardArray;
+   return boardArray;
  }
 
  Game.prototype.toHash = function(boardArray) {
-   var BoardHash = {};
-   BoardHash['a'] = boardArray[0];
+   var boardHash = {};
+   boardHash['a'] = boardArray[0];
    var c = 'a'
     for (i=1; i<=15; i++) {
       c = nextChar(c);
-      BoardHash[c] = boardArray[i];
+      boardHash[c] = boardArray[i];
    }
-   return BoardHash;
+   return boardHash;
 }
 
 function nextChar(c) {
