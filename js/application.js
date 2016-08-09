65cf4a0a7c7d6a185d7035b04427af19d952a9da
Jason Sutter <jasonsutter87@gmail.com>
2016-08-09 00:12:45 -0700

    got the right swipe to work

--- js/application.js
+++ js/application.js
@@ -3,7 +3,6 @@ $(document).ready(function() {
 	console.log(game)
 	board = game.generateBoard()
 	console.log(board)
-	console.log(game.nestedArray(board))
 });
 
 
@@ -13,21 +12,25 @@ function startBoard(){
 	var elements = document.getElementsByClassName('box');
 	for(i=0;i<elements.length; i++){
 		$(elements[i]).css('background-color', 'white')
-		$(elements[i]).html("<h1>"+ board[i]+"</h1>")
+		$(elements[i]).html(board[i])
 	}
 }
 
 
 
 function updateBoard(board){
+	console.log(board)
 	var elements = document.getElementsByClassName('box');
 	
 	for(i=0;i<elements.length; i++){
-		
+		console.log(i)
+		console.log(elements[i])
 		$(elements[i]).css('background-color', 'white')
-		console.log($(elements[i]).html())
-		$(elements[i]).html(board[i])
+		// console.log($(elements[i]).html())
+		console.log('break')
 		console.log(board[i])
+		$(elements[i]).html(board[i])
+		console.log('done')
 
 	}
 
@@ -45,16 +48,14 @@ function keyDown(e) {
 			break;
 		case 100:
 			console.log("D pressed")
-			// board = []
-			// board = board.concat(game.shiftRight(game.nestedArray(board)[0]))
-			// board = board.concat(game.shiftRight(game.nestedArray(board)[1]))
-			// board = board.concat(game.shiftRight(game.nestedArray(board)[2]))
-			// board = board.concat(game.shiftRight(game.nestedArray(board)[3]))
-			// console.log(board)
-			console.log( game.shiftRight(game.nestedArray(board)[0]) );
-				
-
-			// updateBoard()
+			a = game.shiftRight(game.nestedArray(board)[0])
+			b = game.shiftRight(game.nestedArray(board)[1])
+			newBoard1 = a.concat(b);
+			c = game.shiftRight(game.nestedArray(board)[2])
+			d = game.shiftRight(game.nestedArray(board)[3])
+			newBoard2 = c.concat(d);
+			board = newBoard1.concat(newBoard2)
+			updateBoard(board)
 			break;
 		case 115:
 			console.log("S pressed")
