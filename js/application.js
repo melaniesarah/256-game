7dc2ff34546bbe70c1c81e09e947cbf1d44603fa
Jason Sutter <jasonsutter87@gmail.com>
2016-09-20 23:11:29 -0700

    tiles now have colors

--- js/application.js
+++ js/application.js
@@ -4,13 +4,19 @@ $(document).ready(function() {
 });
 
 
-
-
 function startBoard(){
   var elements = document.getElementsByClassName('box') ;
   for(i=0;i<elements.length; i++){
-    $(elements[i]).css('background-color', 'white')
-    $(elements[i]).append(game.board[i])
+       if(game.board[i] == 0){
+          $(elements[i]).css('background-color', 'tan')  
+          $(elements[i]).append('<h1 style="color: tan;">' + game.board[i]+ '</h1>')
+       }else if(game.board[i] == 2){
+          $(elements[i]).css('background-color', 'green')
+          $(elements[i]).append('<h1 style="color: white;">' + game.board[i]+ '</h1>')
+       }else{
+          $(elements[i]).css('background-color', 'blue')
+          $(elements[i]).append('<h1 style="color: white;">' + game.board[i]+ '</h1>')
+       }
   }
 }
 
@@ -21,12 +27,34 @@ function updateBoard(board){
   var elements = document.getElementsByClassName('box');
 
   for(i=0;i<elements.length; i++){
-    $(elements[i]).css('background-color', 'white')
-    // console.log($(elements[i]).html())
-    // console.log('break')
-    // console.log(board[i])
-    $(elements[i]).html('<h1>' + board[i] + '</h1>')
-    // console.log('done')
+    
+     if(game.board[i] == 0){
+          $(elements[i]).css('background-color', 'tan')  
+          $(elements[i]).html('<h1 style="color: tan;">' + board[i] + '</h1>')
+       }else if(game.board[i] == 2){
+          $(elements[i]).css('background-color', 'green')
+          $(elements[i]).html('<h1 style="color: white;">' + board[i] + '</h1>')
+       }else if(game.board[i] == 4){
+          $(elements[i]).css('background-color', 'blue')
+          $(elements[i]).html('<h1 style="color: white;">' + board[i] + '</h1>')
+       }else if(game.board[i] == 8){
+          $(elements[i]).css('background-color', 'grey')
+          $(elements[i]).html('<h1 style="color: white;">' + board[i] + '</h1>')
+       }else if(game.board[i] == 16){
+          $(elements[i]).css('background-color', 'red')
+          $(elements[i]).html('<h1 style="color: white;">' + board[i] + '</h1>')
+       }else if(game.board[i] == 32){
+          $(elements[i]).css('background-color', 'purple')
+          $(elements[i]).html('<h1 style="color: white;">' + board[i] + '</h1>')
+       }else if(game.board[i] == 64){
+          $(elements[i]).css('background-color', 'orange')
+          $(elements[i]).html('<h1 style="color: white;">' + board[i] + '</h1>')
+       }else if(game.board[i] == 256){
+          $(elements[i]).css('background-color', 'yellow')
+          $(elements[i]).html('<h1 style="color: black;">' + board[i] + '</h1>')
+       }else{
+        $(elements[i]).css('background-color', 'pink')
+       }
 
   }
 
@@ -56,13 +84,14 @@ function keyDown(e) {
     case 114:
       console.log("R pressed")
       startBoard();
+      break;
     case 115:
       console.log("S pressed")
       game.swipeDown();
       updateBoard(game.board);
       game.newTile()
       break;
-      default:
+    default:
       console.log("Please use the 'A', 'W','D', or 'S' on the keyboard")
   }
 }
